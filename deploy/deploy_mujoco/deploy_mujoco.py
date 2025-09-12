fd257b9f66dbfa35a16faf13dac8c73129b413e6
myc <mayichao61@163.com>
2025-09-13 00:25:11 +0800

    test:验证关节初始角度

--- deploy/deploy_mujoco/deploy_mujoco.py
+++ deploy/deploy_mujoco/deploy_mujoco.py
@@ -40,7 +40,6 @@ if __name__ == "__main__":
         config = yaml.load(f, Loader=yaml.FullLoader)
         policy_path = config["policy_path"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
         xml_path = config["xml_path"].replace("{LEGGED_GYM_ROOT_DIR}", LEGGED_GYM_ROOT_DIR)
-
         simulation_duration = config["simulation_duration"]
         simulation_dt = config["simulation_dt"]
         control_decimation = config["control_decimation"]
@@ -78,8 +77,11 @@ if __name__ == "__main__":
     m = mujoco.MjModel.from_xml_path(xml_path)
     d = mujoco.MjData(m)
     m.opt.timestep = simulation_dt
-    print("Actuator force range:", m.actuator_ctrlrange)
-    # load policy
+
+    joint_start = 7
+    joint_end = joint_start + len(default_angles)
+    d.qpos[joint_start:joint_end] = default_angles
+    mujoco.mj_forward(m, d)
     policy = torch.jit.load(policy_path)
     with mujoco.viewer.launch_passive(m, d) as viewer:
         start = time.time()
@@ -89,27 +91,21 @@ if __name__ == "__main__":
             joint_end = joint_start + len(target_dof_pos)
             joint_vel_start = 6
             joint_vel_end = joint_vel_start + len(target_dof_pos)
-            # tau = pd_control(
-            #     target_dof_pos,
-            #     d.qpos[joint_start:joint_end],
-            #     kps,
-            #     np.zeros_like(kds),
-            #     d.qvel[joint_vel_start:joint_vel_end],
-            #     kds
-            # )
-            # d.ctrl[:] = tau
-
-            fixed_torque = np.array([1000.0] * len(d.ctrl), dtype=np.float32)  # 为每个关节设置固定力矩
-            d.ctrl[:] = fixed_torque
-            # print("Applied fixed torques:", fixed_torque)
-            # print("Applied torques:", tau)
-            # print("Actual torques:", d.actuator_force)
-            #print("Target positions:", target_dof_pos)
+            tau = pd_control(
+                target_dof_pos,
+                d.qpos[joint_start:joint_end],
+                kps,
+                np.zeros_like(kds),
+                d.qvel[joint_vel_start:joint_vel_end],
+                kds
+            )
+            print("d.qpos:", d.qpos[joint_start:joint_end])
+            print("target_dof_pos:", target_dof_pos)
+            print("d.qvel:", d.qvel[joint_vel_start:joint_vel_end])
+            d.ctrl[:] = tau
+            print("tau:", tau)
             mujoco.mj_step(m, d)
-            #print("Current joint positions (qpos):", d.qpos[joint_start:joint_end])
             counter += 1
-            # print("Current joint positions (qpos):", d.qpos[joint_start:joint_end])
-            # print("Current joint velocities (qvel):", d.qvel[joint_vel_start:joint_vel_end])
             if counter % control_decimation == 0:
                 # Apply control signal here.
 
@@ -121,7 +117,7 @@ if __name__ == "__main__":
                 dqj = dqj * dof_vel_scale
                 gravity_orientation = get_gravity_orientation(quat)
                 omega = omega * ang_vel_scale
-                lin_vel = d.qvel[:3]  # 提取底座线速度
+                lin_vel = d.qvel[:3]  
                 lin_vel = lin_vel * lin_vel_scale  
                 period = 0.8
                 count = counter * simulation_dt
@@ -138,57 +134,17 @@ if __name__ == "__main__":
                 obs[12 : 12 + num_actions] = qj
                 obs[12 + num_actions : 12 + 2 * num_actions] = dqj
                 obs[12 + 2 * num_actions : 12 + 3 * num_actions] = action  # 上一步裁剪后的action
-                obs[12 + 3 * num_actions : 12 + 3 * num_actions  + num_height] = heights
-                # print("lin_vel:", lin_vel)
-                # print("omega:", omega)
-                # print("gravity_orientation:", gravity_orientation)
-                # print("cmd * cmd_scale:", cmd * cmd_scale)
-                # print("qj (scaled):", qj)
-                # print("dqj (scaled):", dqj)
-                # print("action (previous step):", action)
-                """ 
-                def compute_observations(self):
-                    self.obs_buf = torch.cat((  self.base_lin_vel * self.obs_scales.lin_vel,
-                                                self.base_ang_vel  * self.obs_scales.ang_vel,
-                                                self.projected_gravity,
-                                                self.commands[:, :3] * self.commands_scale,
-                                                (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos,
-                                                self.dof_vel * self.obs_scales.dof_vel,
-                                                self.actions
-                                                ),dim=-1)
-                    # add perceptive inputs if not blind
-
-                    if self.cfg.terrain.measure_heights:
-
-                        heights = torch.clip(self.root_states[:, 2].unsqueeze(1) - 0.5 - self.measured_heights, -1, 1.) * self.obs_scales.height_measurements
-                        
-                        self.obs_buf = torch.cat((self.obs_buf, heights), dim=-1)
-
-                    # add noise if needed
-                    if self.add_noise:
-
-                        self.obs_buf += (2 * torch.rand_like(self.obs_buf) - 1) * self.noise_scale_vec
-
-                            obs_tensor = torch.from_numpy(obs).unsqueeze(0)
-                """
-                add_noise = config.get("add_noise", False)
-                if add_noise:
-                    noise_scales = config.get("noise_scales", {})
-                    noise_scale_vec = np.zeros_like(obs)
-                    noise_level = config.get("noise_level", 1.0)
-                    noise_scale_vec += noise_level * 0.01  # 你可以细分每一段
-                    obs += (2 * np.random.rand(*obs.shape) - 1) * noise_scale_vec
-
+                base_z = d.qpos[2]
+                measured_heights = np.zeros(num_height, dtype=np.float32)  # 平地时全0
+                heights = np.clip(base_z - 0.5 - measured_heights, -1, 1) * height_measurements
+                obs[12 + 3 * num_actions : 12 + 3 * num_actions + num_height] = heights
                 obs = np.clip(obs, -clip_observations, clip_observations)
-                #print("obs:", obs)
                 obs_tensor = torch.from_numpy(obs).unsqueeze(0)
                 #print("obs_tensor:", obs_tensor)
                 action = policy(obs_tensor).detach().numpy().squeeze()
 
                 action = np.clip(action, -clip_actions, clip_actions)
-
-                #print("policy action:", action)
-                #print("default_angles:", default_angles) 
+                #print("action:", action*action_scale)
                 target_dof_pos = action * action_scale + default_angles  # 策略输出的是增量 并放缩+初始角度
                 #print("target_dof_pos:", target_dof_pos)
             viewer.sync()
